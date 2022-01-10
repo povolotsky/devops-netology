3c854ed1910c04c5ba50f38c42846cfca1f9d876
Vadim Povolotsky <vadimpovolotsky@MacBook-Air-Vadim.local>
2022-01-10 16:04:20 +0300

    merge: use shift 10/01/22

--- branching/merge.sh
+++ branching/merge.sh
@@ -2,8 +2,14 @@
 # display command line options
 
 count=1
+<<<<<<< HEAD
 for param in "S*"; do    
     echo "\$* Parameter #$count = $param"
+=======
+while [[ -n "$1" ]]"; do    
+    echo "Parameter #$count = $1"
+    count=$(( $count + 1 ))
+>>>>>>> 796c356 ( merge: use shift 10/01/22)
     shift
 done
 
