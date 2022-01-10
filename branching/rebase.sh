7698c3de0e35012c3b231622d6a215b29e848cb7
Vadim Povolotsky <vadimpovolotsky@MacBook-Air-Vadim.local>
2022-01-10 16:02:32 +0300

    Merge all branches

--- branching/rebase.sh
+++ branching/rebase.sh
@@ -2,8 +2,10 @@
 # display command line options
 # add 10/01/2022
 count=1
-for param in "$*"; do
-    echo "\$* Parameter #$count = $param"
+for param in "$@"; do
+    echo "Parameter : $param"
     count=$(( $count + 1 ))
 done
+
+echo "====="
 
